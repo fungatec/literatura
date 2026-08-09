c8fbae6d62491843a591479565bd5e121ad8b2bc
FungaTec <fungatec34@gmail.com>
2026-08-08 19:12:37 -0600

    Change GOOGLE_DRIVE_FOLDER to a new link
    Updated GOOGLE_DRIVE_FOLDER constant to a new value.

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
  */
 
 // Master Google Drive Link
-const GOOGLE_DRIVE_FOLDER = 'https://drive.google.com/drive/folders/1A2tL3cnp3z78BmFaf4kwec9OO46U6LB1';
+const GOOGLE_DRIVE_FOLDER = 'LNK';
 
 document.addEventListener('DOMContentLoaded', () => {
     // Drive button trigger if needed
